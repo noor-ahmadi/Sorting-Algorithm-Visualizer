733a75fd9e779a36c8717f754ada842fa2719221
noor-ahmadi <noorahmadi014@gmail.com>
2023-09-01 21:39:18 -0400

    browser-changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const app = express();
+app.use(express.static("public"));
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
