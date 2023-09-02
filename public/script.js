2ea91ee84b50756fffb714bb8e7a96c352340bf9
noor-ahmadi <noorahmadi014@gmail.com>
2023-09-01 21:49:56 -0400

    socket-adjustments

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect(window.location.origin);
 const svg = d3.select("#visualization");
 const width = +svg.node().getBoundingClientRect().width;
 const height = +svg.attr("height");
